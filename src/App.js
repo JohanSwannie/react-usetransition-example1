57b5ac1efdee663032e66055b05006042eed8a7d
jswan <jswannieman@gmail.com>
2023-07-21 15:53:04 +1200

    Commit App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-import React, { useState, useTransition } from "react";
+import { useState, useTransition } from "react";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  const [isPending, startTransition] = useTransition();
+  const [myInput, setMyInput] = useState("");
+  const [array, setArray] = useState([]);
+
+  const ARRAY_LENGTH = 12000;
+
+  function resetInput() {
+    setMyInput("");
+    setArray([]);
+    document.getElementById("inputter").focus();
+  }
+
+  function handleChange(event) {
+    event.preventDefault();
+    setMyInput(event.target.value);
+    startTransition(() => {
+      const list = [];
+      for (var i = 0; i < ARRAY_LENGTH; i++) {
+        list.push(event.target.value);
+      }
+      setArray(list);
+    });
+  }
+  return (
+    <div className="App">
+      <button onClick={resetInput}>Reset</button>
+      <input
+        id="inputter"
+        type="text"
+        value={myInput}
+        placeholder="Enter Text"
+        autoFocus
+        onChange={handleChange}
+      />
+      {isPending
+        ? " Input Loading now, please be patient..."
+        : array.map((element, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  fontSize: "60px",
+                  color: "red",
+                  fontFamily: "Tangerine",
+                }}
+              >
+                {element}
+              </div>
+            );
+          })}
+    </div>
+  );
 }
-
-export default App;
